d54dd5c50febbb38b5074290d95a8f74c0980a78
Arian <arian.kulmer@web.de>
2019-08-05 15:27:13 +0200

    BRINGUP don't exclude twrpapp
    Change-Id: I077ed456b4058eb89ab873b1e5e7637827e35763

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,6 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 255
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_EXCLUDE_SUPERSU := true
-TW_EXCLUDE_TWRPAPP := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_INPUT_BLACKLIST := "hbtp_vm"
