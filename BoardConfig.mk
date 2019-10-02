4644d4033063996b22cf2c8d5767fe178c2db17e
Arian <arian.kulmer@web.de>
2019-10-02 23:50:35 +0200

    z3: Exclude TWRP app
    Change-Id: I6e1ec6af714a35f837becf83eb9db06e623e0b13

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,7 @@ RECOVERY_SDCARD_ON_DATA := true
 # TWRP specific build flags
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_CRYPTO := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 255
